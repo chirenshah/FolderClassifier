7cb74338ccc2b1574bc30f224c03a5a4e68e1e6a
Chiren Shah <55741062+chirenshah@users.noreply.github.com>
2020-05-11 06:56:34 +0530

    fixed the bug where files with same name weren't moved

--- move.py
+++ move.py
@@ -39,59 +39,66 @@ def move(folder = get_download_folder()):
     files = os.listdir(folder)
 
     for file in files:
-        if(file.endswith(".pdf")):
-            if(not os.path.exists(folder + "\Pdfs")):
-                os.mkdir(folder + "\Pdfs")
-            try:
-                shutil.move("{}/{}".format(folder, file),"{}/{}".format(folder,"Pdfs"))
-            except:
-                shutil.move("{}/{}({})".format(folder, file,random.randrange(0,10)),"{}/{}".format(folder,"Pdfs"))
+        split = file.split(".")
+        print(len(split))
+        if (len(split)>1):
+            filename = split[0]
+            ext = split[1]
+            if(file.endswith(".pdf") or file.endswith(".html")):
+                if(not os.path.exists(folder + "\Pdfs")):
+                    os.mkdir(folder + "\Pdfs")
+                try:
+                    shutil.move("{}/{}".format(folder, file),"{}/{}".format(folder,"Pdfs"))
+                except:
+                    shutil.move("{}/{}".format(folder,file), "{}/{}/{}({}).{}".format(folder, "Pdfs",filename,random.randrange(0,10),ext))
 
-        if(file.endswith(".exe")):
-            if(not os.path.exists(folder + "\Setups")):
-                os.mkdir(folder + "\Setups")
-            try:
-                shutil.move("{}/{}".format(folder, file),"{}/{}".format(folder,"Setups"))
-            except:
-                shutil.move("{}/{}({})".format(folder, file,random.randrange(0,10)),"{}/{}".format(folder,"Setups"))
+            if(file.endswith(".exe") or file.endswith(".msi")):
+                if(not os.path.exists(folder + "\Setups")):
+                    os.mkdir(folder + "\Setups")
+                try:
+                    shutil.move("{}/{}".format(folder, file),"{}/{}".format(folder,"Setups"))
+                except:
+                   shutil.move("{}/{}".format(folder,file), "{}/{}/{}({}).{}".format(folder, "Setups",filename,random.randrange(0,10),ext))
 
-        if(file.endswith(".torrent")):
-            if(not os.path.exists(folder + "\Torrents")):
-                os.mkdir(folder + "\Torrent")
-            try:
-                shutil.move("{}/{}".format(folder, file),"{}/{}".format(folder,"Torrents"))
-            except:
-                shutil.move("{}/{}({})".format(folder, file,random.randrange(0,10)),"{}/{}".format(folder,"Torrents"))
+            if(file.endswith(".torrent")):
+                if(not os.path.exists(folder + "\Torrents")):
+                    os.mkdir(folder + "\Torrent")
+                try:
+                    shutil.move("{}/{}".format(folder, file),"{}/{}".format(folder,"Torrents"))
+                except:
+                    shutil.move("{}/{}".format(folder,file), "{}/{}/{}({}).{}".format(folder, "Torrents",filename,random.randrange(0,10),ext))
 
-        if(file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg")):
-            if(not os.path.exists(folder + "\Images")):
-                os.mkdir(folder + "\Images")
-            try:
-                shutil.move("{}/{}".format(folder, file),"{}/{}".format(folder,"Images"))
-            except:
-                shutil.move("{}/{}({})".format(folder, file,random.randrange(0,10)),"{}/{}".format(folder,"Images"))
+            if(file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg")):
+                if(not os.path.exists(folder + "\Images")):
+                    os.mkdir(folder + "\Images")
+                try:
+                    shutil.move("{}/{}".format(folder, file),"{}/{}".format(folder,"Images"))
+                except:
+                    shutil.move("{}/{}".format(folder,file), "{}/{}/{}({}).{}".format(folder, "Images",filename,random.randrange(0,10),ext))
 
-        if(ifexist(file)):
-            if(not os.path.exists(folder + "\Video")):
-                os.mkdir(folder + "\Video")
-            try:
-                shutil.move("{}/{}".format(folder, file),"{}/{}".format(folder,"Video"))
-            except:
-                shutil.move("{}/{}({})".format(folder, file,random.randrange(0,10)),"{}/{}".format(folder,"Video"))   
-        if(file.endswith(".zip")):
-            if(not os.path.exists(folder + "\Zipfile")):
-                os.mkdir(folder + "\Zipfile")
-            try:
-                shutil.move("{}/{}".format(folder, file),"{}/{}".format(folder,"Zipfile"))
-            except:
-                shutil.move("{}/{}({})".format(folder, file,random.randrange(0,10)),"{}/{}".format(folder,"Zipfile"))
-        if(file.endswith(".xls") or file.endswith(".txt")):
-            if(not os.path.exists(folder + "\Textfiles")):
-                os.mkdir(folder + "\Textfiles")
-            try:
-                shutil.move("{}/{}".format(folder, file),"{}/{}".format(folder,"Textfiles"))
-            except:
-                shutil.move("{}/{}({})".format(folder, file,random.randrange(0,10)),"{}/{}".format(folder,"Textfiles"))   
+            if(ifexist(file)):
+                if(not os.path.exists(folder + "\Video")):
+                    os.mkdir(folder + "\Video")
+                try:
+                    shutil.move("{}/{}".format(folder, file),"{}/{}".format(folder,"Video"))
+                except:
+                    shutil.move("{}/{}".format(folder,file), "{}/{}/{}({}).{}".format(folder, "Video",filename,random.randrange(0,10),ext))
+                       
+            if(file.endswith(".zip")):
+                if(not os.path.exists(folder + "\Zipfile")):
+                    os.mkdir(folder + "\Zipfile")
+                try:
+                    shutil.move("{}/{}".format(folder, file),"{}/{}".format(folder,"Zipfile"))
+                except:
+                    shutil.move("{}/{}".format(folder,file), "{}/{}/{}({}).{}".format(folder, "Zipfile",filename,random.randrange(0,10),ext))
+                    
+            if(file.endswith(".xls") or file.endswith(".txt")):
+                if(not os.path.exists(folder + "\Textfiles")):
+                    os.mkdir(folder + "\Textfiles")
+                try:
+                    shutil.move("{}/{}".format(folder, file),"{}/{}".format(folder,"Textfiles"))
+                except:
+                   shutil.move("{}/{}".format(folder,file), "{}/{}/{}({}).{}".format(folder, "Textfiles",filename,random.randrange(0,10),ext))   
         
 if __name__ == "__main__":
     systemArgument()
